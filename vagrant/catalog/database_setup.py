876d5837ff5ecb4e670f03c991223bbc161bf152
Eddie Warner <happyspace@gmail.com>
2016-01-12 16:08:10 -0800

    Create database. Add requirements to support virtual env.

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+engine = create_engine("postgres://localhost/catalog")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+print(database_exists(engine.url))
