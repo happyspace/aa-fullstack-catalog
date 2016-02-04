c9cb19e2e4bed46aaee0267b19a8e8ea8527c61e
Eddie Warner <happyspace@gmail.com>
2016-02-04 08:51:51 -0800

    Add support for ES6+. Convert to ES6 classes with property initializers.

---
+++ vagrant/flask-react/project/static/src/tutorial.js
@@ -0,0 +1,56 @@
+
+class Comment extends React.Component {
+    static propTypes = {
+        author: React.PropTypes.string.isRequired,
+        children: React.PropTypes.array.isRequired,
+    };
+    render(){
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                { this.props.children }
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    render(){
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component{
+    render() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+}
+
+class CommentBox extends React.Component{
+    render(){
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('commentBox')
+);
+
