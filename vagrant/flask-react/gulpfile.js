526c8fa3157327faec79aad90afa2076411d5870
Eddie Warner <happyspace@gmail.com>
2016-02-16 10:47:31 -0800

    Add gulpfile.js. Use babel plug-in for build.

--- vagrant/flask-react/gulpfile.js
+++ vagrant/flask-react/gulpfile.js
@@ -1,7 +1,21 @@
 
 'use strict';
 
-let gulp = require('gulp');
+const gulp = require('gulp');
+const size = require('gulp-size');
+const babel = require('gulp-babel');
+
+gulp.task(
+    'transform', () => {
+        gulp.src('project/static/src/*.js')
+            .pipe(
+                babel({
+                    presets: ['es2015', 'react', 'stage-0']
+                }))
+            .pipe(gulp.dest('project/static/build'))
+            .pipe(size())
+    }
+);
 
 gulp.task(
     'default', () => {
