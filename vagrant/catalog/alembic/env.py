33511a2b5bcbae45a4975d46808f1b69cb95810b
Eddie Warner <happyspace@gmail.com>
2016-02-24 11:31:59 -0800

    Add model to alembic env.

--- vagrant/catalog/alembic/env.py
+++ vagrant/catalog/alembic/env.py
@@ -1,7 +1,12 @@
 from __future__ import with_statement
+import sys, os, inspect
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from database_setup import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,7 +20,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,8 +1,11 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Unicode, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy import create_engine
+import datetime
 
 Base = declarative_base()
 
@@ -17,9 +20,19 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    email = Column(String(254), nullable=False)
-    picture = Column(String(250))
+    name = Column(Unicode(80), nullable=False)
+    email = Column(Unicode(254), nullable=False)
+    picture = Column(Unicode(250))
+    create_date = Column('create_date', DateTime, default=datetime.datetime.now, nullable=False)
+    last_update = Column('last_update', DateTime, default=datetime.datetime.now, nullable=False)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(50), nullable=False)
+    description = Column(Unicode(200))
 
 
 Base.metadata.create_all(engine)
